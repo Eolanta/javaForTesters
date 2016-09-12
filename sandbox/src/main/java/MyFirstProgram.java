e9c18b5c006aae507eb963237840c204591c1465
Eolanta <deverra@yandex.ru>
2016-09-12 23:16:18 +0300

    Изменен текст

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,6 +2,6 @@ public class MyFirstProgram
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world! First task complete.");
 	}
 }
